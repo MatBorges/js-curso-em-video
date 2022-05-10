f253cf69311c61e965c3919e50e12febd505abbd
Eli Schuindt <eli1968ro@gmail.com>
2022-05-10 10:16:45 -0400

    Exercicio 12

--- Aula12ex/ex012/script.js
+++ Aula12ex/ex012/script.js
@@ -8,17 +8,17 @@ function carregar(){
     horario.innerText = `Agora são ${hora} horas`    
 
     if(hora >= 5 && hora < 12){
-        imagem.src = 'img/manha.png'
+        imagem.src = 'img/manha.jpg'
         imagem.alt = 'Bom Dia!!'
         msg.innerText = `Tenha um Bom Dia!!`
         document.body.style.background = "rgb(255, 240, 158)"
     }else if(hora >= 12 && hora <= 18){
-        imagem.src = 'img/tarde.png'
+        imagem.src = 'img/tarde.jpg'
         imagem.alt = 'Boa Tarde!!'
         msg.innerText = `Tenha uma Boa Tarde!!`
         document.body.style.background = 'rgb(255, 144, 41)'
     }else{
-        imagem.src = 'img/noite.png'
+        imagem.src = 'img/noite.jpg'
         imagem.alt = 'Boa Noite!!'
         msg.innerText = `Tenha uma Boa Noite!!`
         document.body.style.background = 'rgb(0, 85, 160)'
