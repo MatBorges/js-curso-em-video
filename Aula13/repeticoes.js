813f25c635a589ca15d96e95aea7b5d8a5f630b6
Mateus Borges <mateusgaspio@gmail.com>
2022-05-13 01:38:38 -0400

    Aula 13 Repetições

---
+++ Aula13/repeticoes.js
@@ -0,0 +1,11 @@
+// var cont = 1
+// while(cont <= 5){
+//     console.log(`Passou ${cont} vez`)
+//     cont++
+// }
+
+var cont2 = 1
+do{
+    console.log(`passou ${cont2} vez`)
+    cont2++
+}while(cont2 <=5)
