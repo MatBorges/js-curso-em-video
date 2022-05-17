b488c7757583a0b2ca657d9e36f8f1916a106bab
Mateus Borges <mateusgaspio@gmail.com>
2022-05-17 08:21:50 -0400

    ex04

---
+++ Exercícios Repetições/ex04/script.js
@@ -0,0 +1,16 @@
+function calcular() {
+    let num = document.querySelector('input#num')
+    let tab = document.querySelector('select#tabuada')
+
+    if (num.value.length == 0) {
+        alert('Digite um número!!')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            tab.appendChild(item)
+        }
+    }
+}
